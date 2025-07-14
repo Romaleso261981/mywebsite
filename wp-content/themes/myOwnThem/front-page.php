4459022e89cd3a3ca2e585d066cd7c78fe6664e1
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-07-14 13:55:23 +0300

    feat: added ACF

--- wp-content/themes/myOwnThem/front-page.php
+++ wp-content/themes/myOwnThem/front-page.php
@@ -2,50 +2,90 @@
 
 <body>
 
-  <div class="resume">
-    <div class="header">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/avatar.png" alt="Mike">
-      <div>
-        <h1>Hello, I’m Mike,<br>Web Developer</h1>
-        <p>I specialize in building user-friendly websites and custom WordPress solutions.
-          With a keen eye for detail and a passion for problem-solving, I strive to create seamless and efficient
-          digital experiences.</p>
-      </div>
+    <div class="resume">
+        <div class="header">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/avatar.png" alt="Mike">
+            <div>
+                <?php if (get_field('main_title')): ?>
+                <h1><?php the_field('main_title'); ?><br><?php if (get_field('sub_title')): ?>
+                    <?php the_field('sub_title'); ?>
+                    <?php endif; ?></br>
+                </h1>
+                <?php endif; ?>
+                <?php if (get_field('description')): ?>
+                <p><?php the_field('description'); ?></p>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <div class="contacts">
+            <?php if (get_field('contacts_title')): ?>
+            <h3><?php the_field('contacts_title'); ?></h3>
+            <?php endif; ?>
+            <?php if (get_field('phone')): ?>
+            <p><strong>Phone:</strong> <?php the_field('phone'); ?></p>
+            <?php endif; ?>
+            <?php if (get_field('email')): ?>
+            <p><strong>Email:</strong> <?php the_field('email'); ?></p>
+            <?php endif; ?>
+        </div>
+        <h2>Projects</h2>
+        <ol>
+            <li><a href="<?php if (get_field('project_1_link')): ?>
+                    <?php the_field('project_1_link'); ?>
+                    <?php endif; ?>">
+                    <?php if (get_field('project_1_link')): ?>
+                    <?php the_field('project_1_link'); ?>
+                    <?php endif; ?>
+                </a>
+                <?php if (get_field('project_1_title')): ?>
+                <?php the_field('project_1_title'); ?>
+                <?php endif; ?>
+            </li>
+            <li><a href="<?php if (get_field('project_2_link')): ?>
+                    <?php the_field('project_2_link'); ?>
+                    <?php endif; ?>">
+                    <?php if (get_field('project_2_link')): ?>
+                    <?php the_field('project_2_link'); ?>
+                    <?php endif; ?>
+                </a>
+                <?php if (get_field('project_2_title')): ?>
+                <?php the_field('project_2_title'); ?>
+                <?php endif; ?>
+            </li>
+            <li><a href="<?php if (get_field('project_3_link')): ?>
+                    <?php the_field('project_3_link'); ?>
+                    <?php endif; ?>">
+                    <?php if (get_field('project_3_link')): ?>
+                    <?php the_field('project_3_link'); ?>
+                    <?php endif; ?>
+                </a>
+                <?php if (get_field('project_3_title')): ?>
+                <?php the_field('project_3_title'); ?>
+                <?php endif; ?>
+            </li>
+        </ol>
+
+        <h2>Soft Skills</h2>
+        <ul>
+            <li>Problem-Solving</li>
+            <li>Teamwork</li>
+            <li>Communication</li>
+            <li>Adaptability</li>
+            <li>Time Management</li>
+            <li>Attention to Detail</li>
+        </ul>
+
+        <h2>Hard Skills</h2>
+        <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>React.js</li>
+            <li>Node.js</li>
+            <li>Git</li>
+        </ul>
     </div>
-
-    <div class="contacts">
-
-      <h3>Contacts</h3>
-      <p><strong>Phone:</strong> +1 (555) 123-4567</p>
-      <p><strong>Email:</strong> mike@gmail.com</p>
-    </div>
-    <h2>Projects</h2>
-    <ol>
-      <li><a href="https://mikeivanov.com">https://mikeivanov.com</a> — HTML5, CSS3, JavaScript</li>
-      <li><a href="https://mikeivanov.com">https://mikeivanov.com</a> — React.js, Node.js, MongoDB</li>
-      <li><a href="https://mikeivanov.com">https://mikeivanov.com</a> — HTML5, CSS3, JavaScript</li>
-    </ol>
-
-    <h2>Soft Skills</h2>
-    <ul>
-      <li>Problem-Solving</li>
-      <li>Teamwork</li>
-      <li>Communication</li>
-      <li>Adaptability</li>
-      <li>Time Management</li>
-      <li>Attention to Detail</li>
-    </ul>
-
-    <h2>Hard Skills</h2>
-    <ul>
-      <li>HTML5</li>
-      <li>CSS3</li>
-      <li>JavaScript</li>
-      <li>React.js</li>
-      <li>Node.js</li>
-      <li>Git</li>
-    </ul>
-  </div>
 
 </body>
 
--- wp-content/themes/myOwnThem/header.php
+++ wp-content/themes/myOwnThem/header.php
@@ -2,14 +2,17 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mike's Resume</title>
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mike's Resume</title>
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
 </head>
 
 <header>
-  <div class="header-content">
-    <h1>My Resume</h1>
-  </div>
+    <div class="header-content">
+        <?php if (get_field('header_title')): ?>
+        <h1><?php the_field('header_title'); ?></h1>
+        <?php endif; ?>
+    </div>
 </header>
