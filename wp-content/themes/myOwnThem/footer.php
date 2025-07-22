333aa105b06629cc706f568f3fee94b6adb36540
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-07-22 10:47:44 +0300

    feat: update footer to include dynamic copyright year and remove Hello World JavaScript file

--- wp-content/themes/myOwnThem/footer.php
+++ wp-content/themes/myOwnThem/footer.php
@@ -1,6 +1,7 @@
 <footer>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/Hello-World.js"></script>
     <div class="footer-content">
-        <p>Copyright &copy; 2025 My Resume</p>
+        <p>Copyright &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
     </div>
 </footer>
 
