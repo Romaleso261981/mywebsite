f4e0884810c811da9796cda124fd22d5f7081f55
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-07-25 11:50:57 +0300

    feat: redesign header and footer for improved layout; enhance front-page structure with new sections for recent posts and featured works; remove unused assets and styles

--- wp-content/themes/myOwnThem/front-page.php
+++ wp-content/themes/myOwnThem/front-page.php
@@ -1,119 +1,86 @@
 <?php get_header(); ?>
-
-<body>
-
-    <div class="resume">
-        <?php echo custom_welcome_message(); ?>
-        <div class="header">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/avatar.png" alt="Mike">
-            <div>
-                <?php if (get_field('main_title')): ?>
-                <h1><?php the_field('main_title'); ?><br><?php if (get_field('sub_title')): ?>
-                    <?php the_field('sub_title'); ?>
-                    <?php endif; ?></br>
-                </h1>
-                <?php endif; ?>
-                <?php if (get_field('description')): ?>
-                <p>
-                    <?php the_field('description'); ?>
-                </p>
-                <?php endif; ?>
+<main class="main">
+    <section class="john">
+        <div class="container">
+            <div class="john_photo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/john_photo.png" alt="John"
+                    class="john_photo_img">
+            </div>
+            <div class="john_info">
+                <h1 class="john_title"><?php the_field('header_title'); ?></h1>
+                <p class="john_subtitle"><?php the_field('description'); ?></p>
+                <a href="#" class="john_btn">Download resume</a>
             </div>
         </div>
-
-        <div class="contacts">
-            <?php if (get_field('contacts_title')): ?>
-            <h3><?php the_field('contacts_title'); ?></h3>
-            <?php endif; ?>
-            <?php if (get_field('phone')): ?>
-            <p><strong>Phone:</strong> <?php the_field('phone'); ?></p>
-            <?php endif; ?>
-            <?php if (get_field('email')): ?>
-            <p><strong>Email:</strong> <?php the_field('email'); ?></p>
-            <?php endif; ?>
+    </section>
+    <section class="recent">
+        <div class="container">
+            <h2>Recent posts</h2>
+            <a href="#">View all</a>
+            <div class="recent_posts">
+                <article class="recent_posts_article">
+                    <h3 class="article_title">Making a design system from scratch</h3>
+                    <div class="article_meta">
+                        <data>12 Feb 2020</data>|
+                        <p>Design, Pattern</p>
+                    </div>
+                    <p class="article_exerpt">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+                        sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt
+                        nostrud amet.</p>
+                </article>
+                <article class="recent_posts_article">
+                    <h3 class="article_title">Creating pixel perfect icons in Figma</h3>
+                    <div class="article_meta">
+                        <data>12 Feb 2020</data>|
+                        <p>Figma, Icon Design</p>
+                    </div>
+                    <p class="article_exerpt">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+                        sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt
+                        nostrud amet.</p>
+                </article>
+            </div>
         </div>
-        <?php if (have_rows('projects')): ?>
-        <?php custom_heading('Projects'); ?>
-        <?php
-            echo '<ol>';
-            while (the_repeater_field('projects')) {
-                echo '<li>' . "<a>" . get_sub_field('project') . '</a>' . ' ' . 'HTML5, CSS3, JavaScript' . '</li>';
-                echo '<li>' . "<a>" . get_sub_field('project') . '</a>' . ' ' . 'HTML5, CSS3, JavaScript' . '</li>';
-                echo '<li>' . "<a>" . get_sub_field('project') . '</a>' . ' ' . 'HTML5, CSS3, JavaScript' . '</li>';
-            }
-            echo '</ol>';
-            ?>
-        <?php endif; ?>
-        <?php if (have_rows('soft_skills')): ?>
-        <?php custom_heading('Soft Skills'); ?>
-        <?php
-            echo '<ul>';
-            while (the_repeater_field('soft_skills')) {
-                echo '<li>' . get_sub_field('skill') . '</li>';
-                echo '<li>' . get_sub_field('skill2') . '</li>';
-                echo '<li>' . get_sub_field('skill3') . '</li>';
-                echo '<li>' . get_sub_field('skill4') . '</li>';
-            }
-            echo '</ul>';
-            ?>
-        <?php endif; ?>
-
-        <?php if (have_rows('hard_skills')): ?>
-        <?php custom_heading('Hard Skills'); ?>
-        <?php
-            echo '<ul>';
-            while (the_repeater_field('hard_skills')) {
-                echo '<li>' . get_sub_field('skill1') . '</li>';
-                echo '<li>' . get_sub_field('skill2') . '</li>';
-                echo '<li>' . get_sub_field('skill3') . '</li>';
-                echo '<li>' . get_sub_field('skill4') . '</li>';
-            }
-            echo '</ul>';
-            ?>
-        <?php endif; ?>
-
-        <?php custom_heading('Останні записи'); ?>
-        <div class="recent-posts">
-            <?php
-            $recent_posts = new WP_Query(array(
-                'post_type' => 'post',
-                'posts_per_page' => 3,
-                'post_status' => 'publish'
-            ));
-
-            if ($recent_posts->have_posts()) :
-                while ($recent_posts->have_posts()) : $recent_posts->the_post();
-            ?>
-            <article class="post-preview">
-                <h3 class="post-title">
-                    <a href="<?php echo get_permalink(); ?>">
-                        <?php echo esc_html(get_the_title()); ?>
-                    </a>
-                </h3>
-                <div class="post-meta">
-                    <span class="post-date"><?php echo esc_html(get_the_date()); ?></span>
-                    <span class="post-author"><?php echo esc_html(get_the_author()); ?></span>
-                </div>
-                <div class="post-excerpt">
-                    <?php echo esc_html(wp_trim_words(get_the_excerpt(), 20, '...')); ?>
-                </div>
-                <a href="<?php echo esc_url(get_permalink()); ?>" class="read-more">
-                    Читати далі →
-                </a>
-            </article>
-            <?php
-                endwhile;
-                wp_reset_postdata();
-            else :
-                ?>
-            <p>
-                Записів поки немає.
-                <a href="<?php echo esc_url(admin_url('post-new.php')); ?>">Створити перший запис</a>
-            </p>
-            <?php endif; ?>
+    </section>
+    <section class="works">
+        <div class="container">
+            <h2>Featured works</h2>
+            <ul class="work_list">
+                <li class="work_list_item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/work_1.jpg" alt="work image">
+                    <div class="work_info">
+                        <h3>Designing Dashboards</h3>
+                        <a href="#" class="date">2020</a>
+                        <a href="#" class="category">Dashboard</a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis accusantium, sed, rem
+                            neque incidunt, exercitationem eligendi officia quaerat corporis culpa sit debitis
+                            impedit magni vitae eos earum totam animi delectus!</p>
+                    </div>
+                </li>
+                <li class="work_list_item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/work_1.jpg" alt="work image">
+                    <div class="work_info">
+                        <h3>Designing Dashboards</h3>
+                        <a href="#" class="date">2020</a>
+                        <a href="#" class="category">Dashboard</a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis accusantium, sed, rem
+                            neque incidunt, exercitationem eligendi officia quaerat corporis culpa sit debitis
+                            impedit magni vitae eos earum totam animi delectus!</p>
+                    </div>
+                </li>
+                <li class="work_list_item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/work_1.jpg" alt="work image">
+                    <div class="work_info">
+                        <h3>Designing Dashboards</h3>
+                        <a href="#" class="date">2020</a>
+                        <a href="#" class="category">Dashboard</a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis accusantium, sed, rem
+                            neque incidunt, exercitationem eligendi officia quaerat corporis culpa sit debitis
+                            impedit magni vitae eos earum totam animi delectus!</p>
+                    </div>
+                </li>
+            </ul>
         </div>
-    </div>
-
-</body>
+    </section>
+</main>
 
 <?php get_footer(); ?>
