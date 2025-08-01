857092d582ab357f824f287efd176a590baab60c
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-01 10:31:14 +0300

    refactor: remove redundant footer elements to streamline code and improve maintainability

--- wp-content/themes/lidia/footer.php
+++ wp-content/themes/lidia/footer.php
@@ -5,60 +5,7 @@
     ?>
 
     <footer class="footer">
-        <div class="footer__body">
-            <div class="footer__container">
-                <div class="footer__item">
-                    <h4 class="footer__title">
-                        <?php echo $footer_logo['footer_logo_text']; ?>
-                    </h4>
-                    <?php if ($footer_logo['footer_logo']): ?>
-                    <?php echo wp_get_attachment_image(
-                            $footer_logo['footer_logo'],
-                            "medium",
-                            false,
-                            ['class' => 'footer__logo']
-                        )
-                        ?>
-                    <?php endif; ?>
-                </div>
-                <div class="footer__item">
-                    <h4 class="footer__title">
-                        <?php if ($footer_logo['footer_logo']): ?>
-                        <?php echo $footer_socials['footer_socials_title']; ?>
-                        <?php endif; ?>
-                    </h4>
-                    <div class="footer__social social">
-                        <?php
-                        if ($footer_socials['social_item']):
-                            foreach ($footer_socials['social_item'] as $social_item):
-                                if ($social_item['social_item_image']):
-                        ?>
-                        <a href="#" class="social__item" target="_blank">
-                            <?php echo wp_get_attachment_image($social_item['social_item_image'], 'thumbnail', false, ['alt' => 'Social Media Icon']); ?>
-                        </a>
-                        <?php
-                                endif;
-                            endforeach;
-                        endif;
-                        ?>
-                    </div>
-                </div>
-                <div class="footer__item">
-                    <h4 class="footer__title">
-                        <?php if ($footer_logo['footer_logo']): ?>
-                        <?php echo $footer_slogan['footer_slogan_title']; ?>
-                        <?php endif; ?>
-                    </h4>
-                    <a href="#" class="footer__link">
-                        <?php if ($footer_slogan['footer_slogan_text']): ?>
-                        <?php echo $footer_slogan['footer_slogan_text']; ?>
-                        <?php endif; ?>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="footer__copy"> &copy; <?php echo date('Y'); ?> <?php echo bloginfo('name'); ?> All rights reserved.
-        </div>
+
     </footer>
     </div>
 
