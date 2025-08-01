8bb290f796caba8856ec0a3de0f9da1542a553a7
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-01 17:55:37 +0300

    feat: add dynamic hero section to front page template; implement conditional rendering for blocks to enhance layout flexibility

--- wp-content/themes/lidia/page-front.php
+++ wp-content/themes/lidia/page-front.php
@@ -1,6 +1,21 @@
 <?php get_header(); ?>
 
 <main class="page">
+    <?php if (have_rows('blocks')) : ?>
+    <?php while (have_rows('blocks')) : the_row(); ?>
+    <?php if (get_row_layout() == 'hero') : ?>
+    <div class="hero">
+        <div class="hero__container">
+            <div class="hero__body">
+                <div class="hero__content">
+                    <h1 class="hero__title"><?php the_sub_field('title'); ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+    <?php endwhile; ?>
+    <?php else: ?>
     <div class="page__container">
         <div class="page__body">
             <div class="page__content">
@@ -8,6 +23,7 @@
             </div>
         </div>
     </div>
+    <?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
