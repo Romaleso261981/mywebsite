41d91428d4c3ecc3b20ef4fe782825bb1f55ac26
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-05 15:30:49 +0300

    fix: refactor reviews section to dynamically render content from ACF fields; address PHP notices related to early translation loading for the ACF domain

--- wp-content/themes/lidia/partials/flexible_blocks/reviews.php
+++ wp-content/themes/lidia/partials/flexible_blocks/reviews.php
@@ -1,59 +1,34 @@
- <section id="reviews" class="page__reviews reviews">
-     <div class="reviews__container">
-         <div class="reviews__header-block header-block header-block--margin">
-             <div class="header-block__label">REVIEWS</div>
-             <h2 class="header-block__title">Reviews of Others</h2>
-         </div>
-         <div class="reviews__body">
-             <div class="reviews__items">
-                 <article class="reviews__item">
-                     <a href="#" class="reviews__link-avatar">
-                         <img src="<?php echo get_template_directory_uri(); ?>/img/reviews/01.jpg" class="reviews__avatar"
-                             alt="Image">
-                     </a>
-                     <div class="reviews__text">
-                         <p>Engaging plot, vivid characters;
-                             a captivating read that
-                             lingers in your thoughts.</p>
-                     </div>
-                     <h4 class="reviews__title">
-                         <a href="#" class="reviews__link-title">Ahmad Saugi</a>
-                     </h4>
-                     <div class="reviews__position">College Student</div>
-                 </article>
-                 <article class="reviews__item">
-                     <a href="#" class="reviews__link-avatar">
-                         <img src="<?php echo get_template_directory_uri(); ?>/img/reviews/02.jpg" class="reviews__avatar"
-                             alt="Image">
-                     </a>
-                     <div class="reviews__text">
-                         <p>Engaging plot, vivid characters;
-                             a captivating read that
-                             lingers in your thoughts.</p>
-                     </div>
-                     <h4 class="reviews__title">
-                         <a href="#" class="reviews__link-title">Ahmad Saugi</a>
-                     </h4>
-                     <div class="reviews__position">College Student</div>
-                 </article>
-                 <article class="reviews__item">
-                     <a href="#" class="reviews__link-avatar">
-                         <img src="<?php echo get_template_directory_uri(); ?>/img/reviews/03.jpg" class="reviews__avatar"
-                             alt="Image">
-                     </a>
-                     <div class="reviews__text">
-                         <p>
-                             Engaging plot, vivid characters;
-                             a captivating read that
-                             lingers in your thoughts.
-                         </p>
-                     </div>
-                     <h4 class="reviews__title">
-                         <a href="#" class="reviews__link-title">Ahmad Saugi</a>
-                     </h4>
-                     <div class="reviews__position">College Student</div>
-                 </article>
-             </div>
-         </div>
-     </div>
- </section>
+<?php
+$title = get_sub_field('reviews_title');
+$cards = get_sub_field('reviews_cards');
+?>
+
+
+<section id="reviews" class="page__reviews reviews">
+  <div class="reviews__container">
+    <div class="reviews__header-block header-block header-block--margin">
+      <div class="header-block__label">REVIEWS</div>
+      <h2 class="header-block__title"><?php echo $title; ?></h2>
+    </div>
+    <div class="reviews__body">
+      <div class="reviews__items">
+        <?php foreach ($cards as $index => $card) : ?>
+        <article class="reviews__item">
+          <a href="<?php echo $card['image_url']; ?>" class="reviews__link-avatar">
+            <?php echo wp_get_attachment_image($card['image'], 'full'); ?>
+          </a>
+          <div class="reviews__text">
+            <p><?php echo $card['card_description']; ?></p>
+          </div>
+          <h4 class="reviews__title">
+            <a href="<?php echo $card['image_url']; ?>"
+              class="reviews__link-title"><?php echo $card['card_title_']; ?></a>
+          </h4>
+          <div class="reviews__position"><?php echo $card['card_sub_title']; ?></div>
+        </article>
+        <?php endforeach; ?>
+
+      </div>
+    </div>
+  </div>
+</section>
