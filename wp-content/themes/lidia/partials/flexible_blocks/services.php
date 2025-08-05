0e721f364f008040c6e04987b39a9b93f9b6b932
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-05 15:20:48 +0300

    fix: refactor services section to dynamically render content from ACF fields; address PHP notices related to early translation loading for the ACF domain

--- wp-content/themes/lidia/partials/flexible_blocks/services.php
+++ wp-content/themes/lidia/partials/flexible_blocks/services.php
@@ -1,63 +1,35 @@
+<?php
+$title = get_sub_field('services_title');
+$cards = get_sub_field('services_card');
+?>
+
+
+
 <section id="services" class="page__services services">
-    <div class="services__container">
-        <div class="services__header-block header-block header-block--margin">
-            <div class="header-block__label">SERVICES</div>
-            <h2 class="header-block__title">The Services for You</h2>
-        </div>
-        <div class="services__items">
-            <article class="services__item">
-                <div class="services__content">
-                    <div class="services__body">
-                        <h4 class="services__title"><span>Rent</span> your favorite book fairly easy on
-                            <span>Lidia</span>!
-                        </h4>
-                        <div class="services__text">
-                            <p>
-                                Viewing, rent, and organize your favorite books has
-                                never been easier. An integrated digital library rent
-                                that’s simple to use, Lidia lets you spend less time
-                                managing your work and more time actually doing it!
-                            </p>
-                            <p>
-                                Effortless rentals, personalized shelves—Lidia
-                                transforms book management, enhancing your
-                                reading experience~
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="services__image">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/01.jpg" class="services__picture"
-                        alt="Image">
-                </div>
-            </article>
-            <article class="services__item">
-                <div class="services__content">
-                    <div class="services__body">
-                        <h4 class="services__title">
-                            Quick Book Rentals: <span>Dive</span> into <span>Reading</span> Instantly
-                        </h4>
-                        <div class="services__text">
-                            <p>
-                                Discover instant literary delight. Access a vast library,
-                                borrow your favorite reads, and dive into captivating
-                                stories within minutes. Reading made quick and easy,
-                                just a click away!
-                            </p>
-                            <p>
-                                Unlock a world of stories effortlessly.
-                                Browse genres, choose, rent in minutes.
-                                Seamlessly manage your reading adventures
-                                with our intuitive platform~
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="services__image">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/services/02.jpg" class="services__picture"
-                        alt="Image">
-                </div>
-            </article>
-        </div>
+  <div class="services__container">
+    <div class="services__header-block header-block header-block--margin">
+      <div class="header-block__label">SERVICES</div>
+      <h2 class="header-block__title"><?php echo $title; ?></h2>
     </div>
+    <div class="services__items">
+      <?php foreach ($cards as $index => $card) : ?>
+      <article class="services__item">
+        <div class="services__content">
+          <div class="services__body">
+            <h4 class="services__title"><?php echo $card['title']; ?></h4>
+            <div class="services__text">
+              <p><?php echo $card['services_description_first_text']; ?></p>
+              <p><?php echo $card['services_description_second_text']; ?></p>
+            </div>
+          </div>
+        </div>
+        <div class="services__image">
+          <?php echo wp_get_attachment_image($card['image'], 'full'); ?>
+        </div>
+      </article>
+      <?php endforeach; ?>
+    </div>
+
+  </div>
+  </div>
 </section>
